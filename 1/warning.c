db40897048ab2c7895a0f10989b369663d51d096
Seth D Zerishnek-Gower <sdg7234@cs.rit.edu>
2019-08-29 09:58:22 -0400

    Initial Commit

---
+++ 1/warning.c
@@ -0,0 +1,31 @@
+/// file: warning.c
+/// description: faulty code example that needs repair.
+/// @author RIT CS Dept.
+
+#include <stdio.h>
+
+#define SIZE 0               ///< size of the array
+
+/// compute and return the year representing the current millenium
+/// using the century parameter to compute the result.
+/// @param century integer number of the current century AD
+/// @return year of the last presidential election
+
+int compute( int century ) {
+    int b = century * 5;
+}
+
+/// main computes and prints the year of the next presidential election.
+/// The function should have no parameters and uses compute for its job.
+/// @return 0 to tell the OS that the process ran to a success conclusion
+
+int main( int argc ) {
+    int x[SIZE] = { 1 };
+    int y = compute( 21 );
+
+    printf( "%d\n", y+x[0] );
+
+    if ( y > 1000 )
+    return 1;
+}
+
