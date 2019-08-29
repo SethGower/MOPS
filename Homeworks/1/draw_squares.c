a26554dcbaa9c0606458d90ff08005417d87bfa8
Seth Gower <sdg7234@rit.edu>
2019-08-29 13:42:08 -0400

    comments shit

--- Homeworks/1/draw_squares.c
+++ Homeworks/1/draw_squares.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+/* draws a square of a given size, indented by a certain amount and created
+ * with a certain character */
 void draw_square(int side, int indent, char ch) {
     int i = 0;
     int j = 0;
+    /* outer loop to perform the rows */
     for (i = 0; i < side; i++) {
+        /* handle the indents */
         for (j = 0; j < indent; j++) {
             putchar(' ');
         }
+        /* handle the columns */
         for (j = 0; j < side; j++) {
             putchar(ch);
         }
