f990d335ef963502739ab38dbf34d9499a9faba9
Seth Gower <sdg7234@rit.edu>
2019-10-10 16:37:40 -0400

    finishes homework 5

--- Homeworks/5/bst.c
+++ Homeworks/5/bst.c
@@ -9,9 +9,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+bool validBST(TreeNode *start, int rootVal);
+
 int main(int argc, char *argv[]) {
     TreeNode *bst = build_list();
-    traverse(bst, INORDER);
+    bool succ = reconstruct(bst);
+    if (succ) {
+        if (NULL != bst) {
+            printf("Preoder Traversal:\n");
+            traverse(bst, PREORDER);
+            printf("Inorder Traversal:\n");
+            traverse(bst, INORDER);
+            printf("Postorder Traversal:\n");
+            traverse(bst, POSTORDER);
+        } else {
+            printf("Empty BST\n");
+        }
+    } else {
+        fprintf(stderr, "Input is not a BST preorder traversal\n");
+        return 1;
+    }
     cleanup(bst);
     return 0;
 }
@@ -35,10 +52,7 @@ TreeNode *build_list(void) {
         if (NULL == head)
             head = curr;
         if (NULL != last) {
-            if (curr->val < last->val)
-                last->left = curr;
-            else
-                last->right = curr;
+            last->left = curr;
         }
         last = curr;
     }
@@ -50,11 +64,57 @@ TreeNode *build_list(void) {
  * Function:         bool reconstruct
  *                   Reconstructs BST from the TreeNode pointer passed in.
  * Where:
- *                   TreeNode *head - TODO
+ *                   TreeNode *head - the current node being reconstructed
  * Return:           Whether or not it failed
  * Error:            False if unable to reconstruct
  *****************************************************************************/
-bool reconstruct(TreeNode *head) { return true; }
+bool reconstruct(TreeNode *head) {
+    TreeNode *curr, *prev;
+    if (NULL == head)
+        return true;
+    curr = head;
+    while (curr) {
+        if (curr->val > head->val) {
+            head->right = curr;
+            prev->left = NULL;
+            break;
+        }
+        prev = curr;
+        curr = curr->left;
+    }
+    reconstruct(head->left);
+    reconstruct(head->right);
+    return validBST(head->right, head->val);
+}
+
+/******************************************************************************
+ * Function:         bool validBST
+ *                   Recursive function to test if BST is valid
+ * Where:
+ *                   TreeNode *start - Node to start checking
+ *                   int rootVal - Overall root value. To check against
+ *
+ * Return:           boolean - true if valid, false if not
+ * Error:            false if not valid
+ *****************************************************************************/
+bool validBST(TreeNode *start, int rootVal) {
+    bool leftGood = false, rightGood = false;
+    if (NULL == start) { /* a NULL node can't be invalid */
+        return true;
+    }
+    /* Check if current value is greater than overall root */
+    if (start->val > rootVal) {
+        leftGood = validBST(start->left, rootVal);
+        rightGood = validBST(start->right, rootVal);
+        /* if not greater, then this value is not good */
+    } else {
+        /* print error output */
+        fprintf(stderr, "Error at value %d\n", start->val);
+        return false;
+    }
+    /* if both branches are valid, then the current is valid */
+    return leftGood && rightGood;
+}
 
 /******************************************************************************
  * Function:         void traverse
@@ -68,10 +128,10 @@ bool reconstruct(TreeNode *head) { return true; }
  *****************************************************************************/
 void traverse(const TreeNode *root, const TraversalType type) {
     if (NULL != root) {
+        /* print according to the TraversalType */
         switch (type) {
         case PREORDER:
             printf("%d\n", root->val);
-            ;
             traverse(root->left, type);
             traverse(root->right, type);
             break;
@@ -92,16 +152,19 @@ void traverse(const TreeNode *root, const TraversalType type) {
  * Function:         void cleanup
  *                   Recursively goes through tree and frees each node.
  * Where:
- *                   TreeNode *root - TODO
+ *                   TreeNode *root - node to cleanup
  * Return:           None
  * Error:            None
  *****************************************************************************/
 void cleanup(TreeNode *root) {
     if (NULL != root) {
+        /* check if the nodes are not NULL */
         if (root->left)
             cleanup(root->left);
         if (root->right)
             cleanup(root->right);
     }
+    /* free current node and set to NULL to remove dangling pointer */
     free(root);
+    root = NULL;
 }
