afb5e97177030f187b54de4b7c7ec506745640aa
Seth Gower <sdg7234@rit.edu>
2019-10-25 15:18:31 -0400

    implements the parse function

--- Projects/2/parser.c
+++ Projects/2/parser.c
@@ -44,17 +44,57 @@ void rep(char *exp) {
  * Return:           Interpretation tree resulting from stack
  * Error:
  *****************************************************************************/
-tree_node_t *parse(stack_t *stack) { return NULL; }
+tree_node_t *parse(stack_t *stack) {
+    if (empty_stack(stack))
+        return NULL;
+    op_type_t op;
+    char *tok = top(stack);
+    pop(stack);
+    char *strtolPtr;
+    tree_node_t *left, *right;
+    exp_type_t type;
+    if (tok[0] == '+')
+        op = ADD_OP;
+    else if (tok[0] == '-')
+        op = SUB_OP;
+    else if (tok[0] == '*')
+        op = MUL_OP;
+    else if (tok[0] == '/')
+        op = DIV_OP;
+    else if (tok[0] == '%')
+        op = MOD_OP;
+    else if (tok[0] == '=')
+        op = ASSIGN_OP;
+    else if (tok[0] == '?')
+        op = Q_OP;
+    else if (tok[0] == ':')
+        op = ALT_OP;
+    else
+        op = NO_OP; /* not handling ternery yet */
 
-/******************************************************************************
- * Function:         tree_node_t *make_parse_tree
- *                   creates the parse tree from the expression
- * Where:
- *                   char *expr - TODO
- * Return:           The parse tree
- * Error:
- *****************************************************************************/
-tree_node_t *make_parse_tree(char *expr) { return NULL; }
+    if (op != NO_OP) { /* if it is an operation */
+        if (op != Q_OP) {
+            right = parse(stack);
+            left = parse(stack);
+            return make_interior(op, tok, left, right);
+        } else {
+            push(stack, ":");
+            right = parse(stack);
+            left = parse(stack);
+            return make_interior(op, tok, left, right);
+        }
+
+    } else { /* if it is a value or symbol */
+        strtol(tok, &strtolPtr, 10);
+        /* if the two pointers are the same, then strtol failed. This means
+         * that the token wasn't a number, meaning it must be a symbol */
+        if (strtolPtr == tok)
+            type = SYMBOL;
+        else
+            type = INTEGER;
+        return make_leaf(type, tok);
+    }
+}
 
 /******************************************************************************
  * Function:         int eval_tree
