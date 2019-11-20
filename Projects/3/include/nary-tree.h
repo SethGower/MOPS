872e79b3693be70d1ba7d114e9aa80c18bc7d53c
Seth Gower <sdg7234@rit.edu>
2019-11-20 11:48:02 -0500

    finishes docstrings for header files

--- Projects/3/include/nary-tree.h
+++ Projects/3/include/nary-tree.h
@@ -15,7 +15,7 @@ typedef struct treeNode {
  * Function:         void freeTree
  *                   Recursively frees the tree given by the tree argument
  * Where:
- *                   treeNode *tree - TODO
+ *                   treeNode *tree - tree that is being freed
  * Return:           None
  * Error:            None
  *****************************************************************************/
@@ -25,7 +25,7 @@ void freeTree(treeNode *tree);
  * Function:         void printTree
  *                   Prints the contents of the tree to stdout
  * Where:
- *                   treeNode *tree - TODO
+ *                   treeNode *tree - tree that is being printed
  * Return:           None
  * Error:            None
  *****************************************************************************/
@@ -35,8 +35,7 @@ void printTree(treeNode *tree);
  * Function:         treeNode *createNode
  *                   Creates a new node in the tree
  * Where:
- *                   treeNode *tree - TODO
- *                   char *name - TODO
+ *                   char *name - root name for the tree
  * Return:           The pointer to the node that was created
  * Error:            returns NULL if an error occured and sets errno
  *****************************************************************************/
@@ -47,8 +46,9 @@ treeNode *createTree(const char *name);
  * Description:      Returns the pointer to the node with the name given as an
  *                   argument
  * Where:
- *                   treeNode *tree - TODO
- *                   char *name - TODO
+ *                   treeNode *tree - tree that is being searched for a
+ *                                    specific name
+ *                   char *name - name to be searched for
  * Return:           the pointer to the node
  * Error:            NULL if there is no node with that name
  *****************************************************************************/
@@ -59,9 +59,9 @@ treeNode *findNode(treeNode *tree, char *name);
  * Description:      Creates a child in the tree, by finding the parent given
  *                   by "parent" and creates a child with the name "child"
  * Where:
- *                   treeNode *tree - TODO
- *                   char *parent - TODO
- *                   char *child - TODO
+ *                   treeNode *tree - tree that is getting the child
+ *                   char *parent - name of the parent to give the child
+ *                   char *child - name of the child
  * Return:           The pointer to the parent node
  * Error:            returns NULL if there was no parent with that name.
  *****************************************************************************/
@@ -71,7 +71,7 @@ treeNode *addChild(treeNode *tree, char *parent, char *child);
  * Function:         size_t treeSize
  *                   Return the number of nodes in the tree
  * Where:
- *                   treeNode *tree - TODO
+ *                   treeNode *tree - tree to check the size of
  * Return:           size_t - number of nodes in tree
  * Error:            None
  *****************************************************************************/
@@ -81,7 +81,8 @@ size_t treeSize(treeNode *tree);
  * Function:         void printNode
  *                   Prints the Name and name of all children of a single node
  * Where:
- *                   treeNode* node - TODO
+ *                   treeNode* node - tree node to print. Only prints info
+ *                                    about current node, doesn't do BFS
  * Return:           None
  * Error:            None
  *****************************************************************************/
@@ -91,7 +92,8 @@ void printNode(treeNode *node);
  * Function:         size_t treeHeight
  *                   Gets the height of the tree given in parameters
  * Where:
- *                   treeNode *tree - TODO
+ *                   treeNode *tree - tree of which the height is being
+ *                                    checked
  * Return:           size_t - the height of the tree
  * Error:            None
  *****************************************************************************/
--- Projects/3/include/queueADT.h
+++ Projects/3/include/queueADT.h
@@ -6,12 +6,76 @@
 
 typedef struct queueStruct *queue_t;
 
+/******************************************************************************
+ * Function:         queue_t que_create
+ *                   Creates an empty queue
+ * Return:           Pointer to the queue_t object
+ * Error:            None
+ *****************************************************************************/
 queue_t que_create();
+
+/******************************************************************************
+ * Function:         void que_destroy
+ *                   Frees all memory associated with a queue
+ * Where:
+ *                   queue_t queue - queue to be destroyed
+ * Return:           None
+ * Error:            None
+ *****************************************************************************/
 void que_destroy(queue_t queue);
+
+/******************************************************************************
+ * Function:         void que_clear
+ *                   Dequeus all elements form the queue
+ * Where:
+ *                   queue_t queue - queue to be cleared
+ * Return:           None
+ * Error:
+ *****************************************************************************/
 void que_clear(queue_t queue);
+
+/******************************************************************************
+ * Function:         void que_enqueue
+ *                   Enques the data into the queue
+ * Where:
+ *                   queue_t queue - queue to add to
+ *                   void *data - a pointer to the data that is being added
+ * Return:           None
+ * Error:
+ *****************************************************************************/
 void que_enqueue(queue_t queue, void *data);
+
+/******************************************************************************
+ * Function:         void *que_dequeue
+ * Description:      Dequeues an item from the queue and returns the item that
+ *                   was removed
+ * Where:
+ *                   queue_t queue - queue from which to remove an item
+ * Return:           Pointer to the data that was removed
+ * Error:            None
+ *****************************************************************************/
 void *que_dequeue(queue_t queue);
+
+/******************************************************************************
+ * Function:         void *que_front
+ * Description:      Returns the pointer that is currently at the front of the
+ *                   queue. Same thing as dequeue just doesn't actually remove
+ *                   it
+ * Where:
+ *                   const queue_t queue - queue to check
+ * Return:           Pointer to the item at the front
+ * Error:            None
+ *****************************************************************************/
 void *que_front(const queue_t queue);
+
+/******************************************************************************
+ * Function:         size_t que_size
+ *                   Returns the number of items in the queue
+ * Where:
+ *                   queue_t queue - queue of which to return the size
+ * Return:           Number of items currently in the queue
+ * Error:
+ *****************************************************************************/
 size_t que_size(queue_t queue);
 
 #endif
