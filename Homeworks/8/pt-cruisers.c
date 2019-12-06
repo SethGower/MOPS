9c7ff6b74957dc055750391c074e881bfee200f0
Seth Gower <sdg7234@rit.edu>
2019-12-06 13:28:21 -0500

    adds error checking

--- Homeworks/8/pt-cruisers.c
+++ Homeworks/8/pt-cruisers.c
@@ -22,46 +22,60 @@ int main(int argc, char *argv[]) {
     bool invalidName = false;
     char *paddedName = malloc(MAX_NAME_LEN + 1);
     paddedName[MAX_NAME_LEN] = '\0';
+    int requiredArgs = 4;
 
     if (argc < 3) {
         fprintf(stderr,
                 "Usage: pt-cruisers [ delay ] racer1 racer2 [racer3...]\n");
     } else {
         delay = strtol(argv[1], &strtolPtr, 10);
-        if (strtolPtr == argv[1]) { /* delay was passed in */
+        if (strtolPtr == argv[1]) { /* no delay was passed in */
             delay = DEFAULT_WAIT;
             firstRacer = 1;
             numRacers = argc - 1;
+            requiredArgs = 3;
         }
-        threads = calloc(numRacers, sizeof(pthread_t));
-        racers = calloc(numRacers, sizeof(racer_t *));
-        init_racers(delay);
-        for (i = 0; i < numRacers; i++) {
-            if (strlen(argv[i + firstRacer]) > MAX_NAME_LEN) {
-                invalidName = true;
-                break;
-            }
-            padName(argv[i + firstRacer], paddedName);
-            racers[i] = make_racer(paddedName, i);
-        }
-        if (!invalidName) {
+        if (delay > 0 && argc >= requiredArgs) {
+            threads = calloc(numRacers, sizeof(pthread_t));
+            racers = calloc(numRacers, sizeof(racer_t *));
+            init_racers(delay);
             for (i = 0; i < numRacers; i++) {
-                rc = pthread_create(&threads[i], NULL, run, (void *)racers[i]);
-                if (rc) {
-                    fprintf(stderr, "ERROR: pthread_create returned %d\n", rc);
+                if (strlen(argv[i + firstRacer]) > MAX_NAME_LEN) {
+                    invalidName = true;
                     break;
                 }
+                padName(argv[i + firstRacer], paddedName);
+                racers[i] = make_racer(paddedName, i);
             }
-            if (!rc) {
-                clear();
+            if (!invalidName) {
                 for (i = 0; i < numRacers; i++) {
-                    pthread_join(threads[i], &retVal);
+                    rc = pthread_create(&threads[i], NULL, run,
+                                        (void *)racers[i]);
+                    if (rc) {
+                        fprintf(stderr, "ERROR: pthread_create returned %d\n",
+                                rc);
+                        break;
+                    }
                 }
-                set_cur_pos(numRacers + 1, 1);
+                if (!rc) {
+                    clear();
+                    for (i = 0; i < numRacers; i++) {
+                        pthread_join(threads[i], &retVal);
+                    }
+                    set_cur_pos(numRacers + 1, 1);
+                }
+            } else {
+                fprintf(stderr,
+                        "Usage: racer names must not exceed length %d.\n",
+                        MAX_NAME_LEN);
             }
+        } else if (delay < 0) {
+            fprintf(stderr, "Delay %ld is invalid.\n", delay);
+            fprintf(stderr,
+                    "Usage: pt-cruisers [ delay ] racer1 racer2 [racer3...]\n");
         } else {
-            fprintf(stderr, "Usage: racer names must not exceed length %d.\n",
-                    MAX_NAME_LEN);
+            fprintf(stderr,
+                    "Usage: pt-cruisers [ delay ] racer1 racer2 [racer3...]\n");
         }
     }
     if (threads)
@@ -72,6 +86,7 @@ int main(int argc, char *argv[]) {
         }
         free(racers);
     }
+    free(paddedName);
 
     return 0;
 }
