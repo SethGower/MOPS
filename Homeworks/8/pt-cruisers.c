dd51ce7e7b4020ecf6a1cd276194a711ca417a2b
Seth Gower <sdg7234@rit.edu>
2019-12-06 14:08:55 -0500

    adds documentation

--- Homeworks/8/pt-cruisers.c
+++ Homeworks/8/pt-cruisers.c
@@ -1,3 +1,10 @@
+/******************************************************************************
+ * File:             pt-cruisers.c
+ *
+ * Author:           Seth Gower
+ * Created:          12/05/19
+ *                   Main module file for the pt-cruisers homework
+ *****************************************************************************/
 #include "display.h"
 #include "racer.h"
 #include <pthread.h>
@@ -12,24 +19,31 @@ int main(int argc, char *argv[]) {
     srand(time(NULL));
     long delay = 69;
     char *strtolPtr;
-    pthread_t *threads = NULL;
-    racer_t **racers = NULL;
-    int firstRacer = 2;
-    int i = 0;
-    int numRacers = argc - 2;
-    int rc = 0;
-    void *retVal;
-    bool invalidName = false;
-    char *paddedName = malloc(MAX_NAME_LEN + 1);
-    paddedName[MAX_NAME_LEN] = '\0';
-    int requiredArgs = 4;
+    pthread_t *threads = NULL; /* array for all of the threads */
+    racer_t **racers = NULL;   /* array for each racer object */
+    int firstRacer = 2; /* which argument the first racer is, defaults to as if
+                           there is a delay */
+    int i = 0;          /* generic loop control variable */
+    int numRacers = argc - 2; /* number of racers. Defaults to if a delay is
+                                 passed in */
+    int rc = 0;               /* return code for thread calls */
+    void *retVal;             /* useless garbage variable */
+    bool invalidName = false; /* did the loop exit because an invalid name? */
+    char *paddedName = malloc(MAX_NAME_LEN + 1); /* variable which has the name
+                                                    with the underscores */
+    int requiredArgs = 4; /* required number of arguments. Defaults to having a
+                             delay passed in */
 
     if (argc < 3) {
+        /* if too few arguments were passed in */
         fprintf(stderr,
                 "Usage: pt-cruisers [ delay ] racer1 racer2 [racer3...]\n");
     } else {
+        /* try converting the first argument to a number.  */
         delay = strtol(argv[1], &strtolPtr, 10);
         if (strtolPtr == argv[1]) { /* no delay was passed in */
+            /* if no delay was passed in, then change all necessary values to
+             * reflect this. */
             delay = DEFAULT_WAIT;
             firstRacer = 1;
             numRacers = argc - 1;
@@ -39,16 +53,23 @@ int main(int argc, char *argv[]) {
             threads = calloc(numRacers, sizeof(pthread_t));
             racers = calloc(numRacers, sizeof(racer_t *));
             init_racers(delay);
+            /* create all of the racer objects */
             for (i = 0; i < numRacers; i++) {
+                /* check if the name valid. else print error message */
                 if (strlen(argv[i + firstRacer]) > MAX_NAME_LEN) {
                     invalidName = true;
                     break;
                 }
+                /* get the name with underscores */
                 padName(argv[i + firstRacer], paddedName);
+                /* create the racer object */
                 racers[i] = make_racer(paddedName, i);
             }
             if (!invalidName) {
+                /* create all of the threads for the individual racers */
                 for (i = 0; i < numRacers; i++) {
+                    /* passes in the run function as the start_routine, and
+                     * the appropriate racer object */
                     rc = pthread_create(&threads[i], NULL, run,
                                         (void *)racers[i]);
                     if (rc) {
@@ -58,10 +79,13 @@ int main(int argc, char *argv[]) {
                     }
                 }
                 if (!rc) {
+                    /* clear the screen and start running the threads */
                     clear();
                     for (i = 0; i < numRacers; i++) {
                         pthread_join(threads[i], &retVal);
                     }
+                    /* this is so the cursor is good for the shell prompt on
+                     * exit */
                     set_cur_pos(numRacers + 1, 1);
                 }
             } else {
@@ -78,6 +102,7 @@ int main(int argc, char *argv[]) {
                     "Usage: pt-cruisers [ delay ] racer1 racer2 [racer3...]\n");
         }
     }
+    /* checks if the arrays have been allocated, if so free them */
     if (threads)
         free(threads);
     if (racers) {
@@ -86,7 +111,7 @@ int main(int argc, char *argv[]) {
         }
         free(racers);
     }
-    free(paddedName);
+    free(paddedName); /* free the buffer for the processed name */
 
     return 0;
 }
--- Homeworks/8/racer.c
+++ Homeworks/8/racer.c
@@ -1,3 +1,10 @@
+/******************************************************************************
+ * File:             racer.c
+ *
+ * Author:           Seth Gower
+ * Created:          12/05/19
+ *                   Racer module file for the pt-cruisers homework
+ *****************************************************************************/
 #define _DEFAULT_SOURCE
 #include "racer.h"
 #include "display.h"
@@ -6,8 +13,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-static long delayMax = DEFAULT_WAIT;
 
+/* max delay for racers */
+static long delayMax = DEFAULT_WAIT;
+/* mutex to prevent threads from writing to display at same time */
 pthread_mutex_t lock;
 
 void init_racers(long milliseconds) {
@@ -15,11 +24,11 @@ void init_racers(long milliseconds) {
     pthread_mutex_init(&lock, NULL);
 }
 racer_t *make_racer(char *name, int position) {
-    racer_t *racer = calloc(1, sizeof(racer_t));
-    racer->row = position;
-    racer->graphic = malloc(strlen(name) + 1);
-    strcpy(racer->graphic, name);
-    racer->dist = 0;
+    racer_t *racer = calloc(1, sizeof(racer_t)); /* racer variable */
+    racer->row = position;                       /* set the row */
+    racer->graphic = malloc(strlen(name) + 1);   /* allocate space for name */
+    strcpy(racer->graphic, name);                /* copy over the name */
+    racer->dist = 1; /* racer starts at starting line */
     return racer;
 }
 void destroy_racer(racer_t *racer) {
@@ -29,9 +38,13 @@ void destroy_racer(racer_t *racer) {
     }
 }
 void putRacer(racer_t *racer, int col) {
+    /* the fact that the coordinates for the display are 1 indexed pissed me
+     * off */
     size_t i = 0;
     set_cur_pos(racer->row + 1, col - 1);
+    /* puts space at current space */
     put(' ');
+    /* places each character in the name on the screen at new col */
     for (i = 0; i < strlen(racer->graphic); i++) {
         set_cur_pos(racer->row + 1, col + i);
         put(racer->graphic[i]);
@@ -40,17 +53,20 @@ void putRacer(racer_t *racer, int col) {
 void *run(void *racer) {
     int lockResult;
     racer_t *temp = (racer_t *)racer;
-    putRacer(temp, 1);
+    putRacer(temp, 1); /* place the race car at the starting line */
     long delay = 0;
     while (temp->dist <= FINISH_LINE) {
         delay = rand() % delayMax;
-        usleep(delay * 1000);
+        usleep(delay * 1000); /* usleep takes microseconds, delay is in milli */
+        /* locks the mutex to disable other threads from writing to display */
         lockResult = pthread_mutex_lock(&lock);
         if (lockResult) {
             perror("entering run function");
             return NULL;
         }
+        /* draws the car to the display */
         putRacer(temp, ++temp->dist);
+        /* unlocks the mutex */
         lockResult = pthread_mutex_unlock(&lock);
         if (lockResult) {
             perror("exiting run function");
